02b7cea6a4007f37234bd555a077736e5e4d7665
Kunal Jain <80086065+kunaljain-git@users.noreply.github.com>
2021-03-11 17:45:38 +0530

    Add files via upload

--- GreatOutdoorApplication/src/main/java/com/capg/team2/goa/service/IProductService.java
+++ GreatOutdoorApplication/src/main/java/com/capg/team2/goa/service/IProductService.java
@@ -1,5 +1,30 @@
 package com.capg.team2.goa.service;
 
-public class IProductService {
+
+
+
+import java.util.List;
+
+import com.capg.team2.goa.entity.ProductEntity;
+
+public interface IProductService
+{
+	public List<ProductEntity> findAllProducts();
+
+	public ProductEntity findByProductId(String id);
+
+ 	public List<ProductEntity> findByProductCategory(String productCategory);
+
+	public ProductEntity addProduct(ProductEntity productEntity) throws ProductException;
+
+	public ProductEntity updateProduct(ProductEntity productEntity) throws ProductException;
+
+	public void updateProductQuantity(Integer quantity,String productId);
+
+	public void deleteByProductId(String id) throws ProductException;
+
+	public List<ProductEntity> search(String keyword);
+
+	public List<ProductEntity> filter(double maxPrice);
 
 }
