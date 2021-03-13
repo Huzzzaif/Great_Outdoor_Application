a1cc55b0b94d658219400a354c18ff5cbe1bc60d
20171CSE0307 <60254990+20171CSE0307@users.noreply.github.com>
2021-03-13 12:32:17 +0530

    Update ICustomerService.java

--- GreatOutdoorApplication/src/main/java/com/capg/team2/goa/service/ICustomerService.java
+++ GreatOutdoorApplication/src/main/java/com/capg/team2/goa/service/ICustomerService.java
@@ -1,5 +1,11 @@
 package com.capg.team2.goa.service;
 
 public interface ICustomerService {
+  
+  public List<Customer> getAllCustomers();
+	public Customer addCustomer(Customer customer) throws CustomerAlreadyExistsException;
+	public Customer updateCustomer(Customer customer) throws CustomerNotFoundException;
+	public Customer removeCustomer(Customer customer) throws CustomerNotFoundException;
+	public Customer viewCustomer(Customer customer) throws CustomerNotFoundException;
 
 }
